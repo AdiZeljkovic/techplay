8e263c915414f2381517e43137430248e38b2187
AdiZeljkovic <adi.zeljkovic@outlook.com>
2026-01-06 16:11:25 +0100

    Fix access_token key in register response

--- backend/app/Http/Controllers/Api/V1/AuthController.php
+++ backend/app/Http/Controllers/Api/V1/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
 
         return $this->created([
             'user' => $user,
-            'token' => $token,
+            'access_token' => $token,
         ], 'User registered successfully');
     }
 
