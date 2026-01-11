c1e14da39f0bdf2b1269efa30da0eafaaabad69c
AdiZeljkovic <adi.zeljkovic@outlook.com>
2026-01-11 16:33:16 +0100

    Replace broken ThreadResource with fresh ForumThreadResource

---
+++ backend/app/Filament/Resources/ForumThreadResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ForumThreadResource\Pages;
+use App\Models\Thread;
+use BackedEnum;
+use Filament\Schemas\Schema;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms;
+
+class ForumThreadResource extends Resource
+{
+    protected static ?string $model = Thread::class;
+
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-chat-bubble-left-right';
+    protected static ?string $navigationGroup = 'Community';
+    protected static ?string $navigationLabel = 'Threads';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Forms\Components\TextInput::make('title')->required(),
+                Forms\Components\Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->required(),
+                Forms\Components\Select::make('author_id')
+                    ->relationship('author', 'username')
+                    ->required(),
+                Forms\Components\RichEditor::make('content')
+                    ->columnSpanFull(),
+                Forms\Components\Toggle::make('is_pinned'),
+                Forms\Components\Toggle::make('is_locked'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                \Filament\Tables\Columns\TextColumn::make('title')
+                    ->searchable()
+                    ->sortable(),
+                \Filament\Tables\Columns\TextColumn::make('category.name')
+                    ->label('Category')
+                    ->sortable(),
+                \Filament\Tables\Columns\TextColumn::make('author.username')
+                    ->label('Author')
+                    ->sortable(),
+                \Filament\Tables\Columns\IconColumn::make('is_pinned')
+                    ->boolean(),
+                \Filament\Tables\Columns\IconColumn::make('is_locked')
+                    ->boolean(),
+                \Filament\Tables\Columns\TextColumn::make('view_count')
+                    ->sortable(),
+                \Filament\Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                \Filament\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                \Filament\Actions\BulkActionGroup::make([
+                    \Filament\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListForumThreads::route('/'),
+            'create' => Pages\CreateForumThread::route('/create'),
+            'edit' => Pages\EditForumThread::route('/{record}/edit'),
+        ];
+    }
+}
