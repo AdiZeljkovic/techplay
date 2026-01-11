a75cdd33111a0300f28b5ed41b33496fd654b4cc
AdiZeljkovic <adi.zeljkovic@outlook.com>
2026-01-11 16:20:17 +0100

    Add SimpleThreadResource for isolation testing

---
+++ backend/app/Filament/Resources/SimpleThreadResource/Pages/EditSimpleThread.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\SimpleThreadResource\Pages;
+
+use App\Filament\Resources\SimpleThreadResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditSimpleThread extends EditRecord
+{
+    protected static string $resource = SimpleThreadResource::class;
+}
