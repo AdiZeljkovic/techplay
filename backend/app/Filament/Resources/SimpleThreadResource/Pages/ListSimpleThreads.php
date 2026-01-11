31cccb59fca481101fb746a3c8a2793bde59b193
AdiZeljkovic <adi.zeljkovic@outlook.com>
2026-01-11 16:38:09 +0100

    Revert: Restore SimpleThreadResource (confirmed working) and set slug to threads

---
+++ backend/app/Filament/Resources/SimpleThreadResource/Pages/ListSimpleThreads.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\SimpleThreadResource\Pages;
+
+use App\Filament\Resources\SimpleThreadResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListSimpleThreads extends ListRecords
+{
+    protected static string $resource = SimpleThreadResource::class;
+}
