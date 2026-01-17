5d70c2715f2ba0b908010b8731fe94f3ac2bff1a
AdiZeljkovic <adi.zeljkovic@outlook.com>
2026-01-17 17:18:48 +0100

    WebP: Add webp_path migration and Media model update

--- backend/app/Models/Media.php
+++ backend/app/Models/Media.php
@@ -11,15 +11,24 @@ class Media extends Model
     protected $fillable = [
         'title',
         'path',
+        'webp_path',
         'alt_text',
         'mime_type',
         'size',
     ];
 
-    protected $appends = ['url'];
+    protected $appends = ['url', 'webp_url'];
 
     public function getUrlAttribute()
     {
         return \Illuminate\Support\Facades\Storage::url($this->path);
     }
+
+    public function getWebpUrlAttribute()
+    {
+        return $this->webp_path
+            ? \Illuminate\Support\Facades\Storage::url($this->webp_path)
+            : null;
+    }
 }
+
