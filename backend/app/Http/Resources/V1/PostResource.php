eb4234db053787ec40329eba4cd0a18e432c6567
AdiZeljkovic <adi.zeljkovic@outlook.com>
2026-01-22 22:03:02 +0100

    chore: remove debug info

--- backend/app/Http/Resources/V1/PostResource.php
+++ backend/app/Http/Resources/V1/PostResource.php
@@ -11,7 +11,7 @@ class PostResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'content' => $this->content . " [DEBUG: P=" . ($this->author->posts_count ?? '?') . " T=" . ($this->author->threads_count ?? '?') . "]",
+            'content' => $this->content,
             'is_solution' => (bool) $this->is_solution,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
