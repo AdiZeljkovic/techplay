18495318f4b88282f1f5dc5fa825f96c94a72a93
AdiZeljkovic <adi.zeljkovic@outlook.com>
2026-01-29 10:50:47 +0100

    fix: Remove duplicate GA4 from GlobalSeo.tsx - using layout.tsx only

--- frontend/components/seo/GlobalSeo.tsx
+++ frontend/components/seo/GlobalSeo.tsx
@@ -36,23 +36,7 @@ export default function GlobalSeo() {
                 `}
             </Script>
 
-            {/* Google Analytics 4 */}
-            {ga4Id && (
-                <>
-                    <Script
-                        src={`https://www.googletagmanager.com/gtag/js?id=${ga4Id}`}
-                        strategy="afterInteractive"
-                    />
-                    <Script id="google-analytics" strategy="afterInteractive">
-                        {`
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', '${ga4Id}');
-                        `}
-                    </Script>
-                </>
-            )}
+            {/* Google Analytics 4 - MOVED TO layout.tsx to avoid conflicts */}
 
             {/* Google Tag Manager */}
             {gtmId && (
